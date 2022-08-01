c726eacee360699da58bd26a1caa62b11067aff7
Adriano Matos <adrnmatos@gmail.com>
2022-08-01 16:38:10 -0400

    ongoing work

--- database.js
+++ database.js
@@ -1,9 +1,11 @@
 const { writeFile, unlink, readFile } = require("fs").promises
+const Hero = require('./hero')
+
 
 const DB_FILE = 'herois.json'
 
 class Database {
-
+    
     async writeDBFile(content) {
         try {
             await writeFile(DB_FILE, content)
@@ -13,7 +15,7 @@ class Database {
             return false
         }
     }
-
+    
     async readDBFile() {
         try {
             const heroList = JSON.parse(await readFile(DB_FILE))
@@ -22,15 +24,72 @@ class Database {
             console.error('readDBFile error: ', error)
         }
     }
-
-    async createHero(newHero) {
+    
+    async findHero(id) {
         try {
             var heroList = await this.readDBFile()
+            let foundHero = heroList.filter((el) => el.id === id)
+
+            console.log('HERO FOUND: ', foundHero)
+            return foundHero;
+        } catch (error) {
+            console.error('findHero error: ', error)
+        }
+    }
+
+    async createHero(heroFields) {
+        try {
+            var heroList = await this.readDBFile()
+            let newHero = new Hero(heroFields.name, heroFields.power)
             heroList.push(newHero)
+
             await this.writeDBFile(JSON.stringify(heroList))
             return true
         } catch (error) {
             console.error('createHero error: ', error)
+            return false
+        }
+    }
+
+    async updateHero(fields, id) {
+        try {
+            let foundHero = await this.findHero(id)
+            if(foundHero) {
+                console.log('foundHero ID: ', foundHero[0].id)
+                await this.removeHero(foundHero.id)
+                let heroList = await this.readDBFile()
+                console.log('heroList after remove: ', heroList)
+                let updatedHero = {
+                    ...foundHero,
+                    fields
+                }
+
+                console.log(updatedHero)
+                heroList.push(updatedHero)
+                await this.writeDBFile(JSON.stringify(heroList))
+                return true
+            } else {
+                return false
+            }
+        } catch (error) {
+            console.error('updateHero error:', error)
+            return false
+        }
+    }
+
+    async removeHero(id) {
+        try {
+            var heroList = await this.readDBFile()
+            var foundHeroId = heroList.findIndex((el) => el.id === id)
+            if(foundHeroId != -1) {
+                heroList.splice(foundHeroId,1)
+                await this.writeDBFile(JSON.stringify(heroList))
+                return true
+            } else {
+                return false            
+            }
+        } catch (error) {
+            console.error('removeHero error: ', error)
             return false
         }
     }
--- test.js
+++ test.js
@@ -7,21 +7,45 @@ const { isTypedArray } = require('util/types')
 
 describe('heroi test-suite', () => {
 
-    // before('should create herois.json file', () => {
+    // create db file
+    // before('should create db file and returns true/false to indicate success/failure', () => {
     //     database.writeDBFile('[]')
     // })
 
-    it('should read herois.json file contents', async () => {
-        const result = await database.readDBFile()
-        assert.ok(result)
+    // read all heroes
+    // it('should read db file and returns the contents in an array', async () => {
+    //     const result = await database.readDBFile()
+    //     assert.ok(result)
+    // })
+
+    // look-up for hero by its id
+    // it('should find a hero by its id and returns the hero object', async () => {
+    //     const result = await database.findHero('85834034-d3f0-4193-9dab-8c70f80042d6')
+    //     assert.ok(result)
+    // })
+
+    // create hero
+    // it('should insert a new hero on the list and returns true/false to indicate success/failure', async () => {
+    //     const newHeroFields = {name: 'batman', power: 'money'}
+    //     let result = await database.createHero(newHeroFields)
+    //     assert.ok(result, true)
+    // })
+
+    // delete hero
+    // it('should remove a hero from db found by its id and returns true/false to indicate success/failure', async () => {
+    //     let result = await database.removeHero('fa41e4b1-5ba2-4033-878e-bcdc8ffc425c')
+    //     assert.ok(result, true)
+    // })
+
+    // update hero
+    it('should update hero fields and returns true/false to indicate success/failure', async () => {
+        let newFields = {power: 'sword'}
+        const result = await database.updateHero(newFields, 'ac0881f1-aa4a-4d4e-8ffc-c97255df83f1')
+        assert.ok(result, true)
     })
 
-    it('should insert a new hero on the list', async () => {
-        const newHero = {name: 'deadpool', power: 'immortal'}
-        await database.createHero(newHero)
-    })
-
-    // after('should remove herois.json file', () => {
+    // clean-up
+    // after('should remove db file and returns true/false to indicate success/failure', async () => {
     //     database.removeDBFile()
     // })
 
